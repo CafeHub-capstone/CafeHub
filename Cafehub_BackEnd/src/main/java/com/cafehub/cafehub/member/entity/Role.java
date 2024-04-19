bcdebe2ddf1d8d331e1977d72ecd6558140403c7
RYUL99 <seunglul1999@gmail.com>
2024-04-19 10:37:22 +0900

    [FIX] Member Enum 생성자 추가

--- Cafehub_BackEnd/src/main/java/com/cafehub/cafehub/member/entity/Role.java
+++ Cafehub_BackEnd/src/main/java/com/cafehub/cafehub/member/entity/Role.java
@@ -1,6 +1,15 @@
 package com.cafehub.cafehub.member.entity;
 
-public enum Role {
+import lombok.Getter;
 
-    ROLE_USER, ROLE_ADMIN;
+@Getter
+public enum Role {
+    USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+
+
+    private final String value;
+
+    Role(String value){
+        this.value = value;
+    }
 }
