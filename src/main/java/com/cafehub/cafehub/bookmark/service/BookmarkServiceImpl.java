bb4fe2a51a06ad91075839b00de6733d7f3fb357
hwaiiinng <welcome-ing@naver.com>
2024-06-03 19:39:28 +0900

    [REFACTOR] 북마크 중복 처리 및 에러코드 추가

--- src/main/java/com/cafehub/cafehub/bookmark/service/BookmarkServiceImpl.java
+++ src/main/java/com/cafehub/cafehub/bookmark/service/BookmarkServiceImpl.java
@@ -6,9 +6,9 @@ import com.cafehub.cafehub.bookmark.request.BookmarkRequestDto;
 import com.cafehub.cafehub.bookmark.response.BookmarkedCafeDetails;
 import com.cafehub.cafehub.cafe.entity.Cafe;
 import com.cafehub.cafehub.cafe.repository.CafeRepository;
+import com.cafehub.cafehub.common.ErrorCode;
 import com.cafehub.cafehub.common.dto.ResponseDto;
 import com.cafehub.cafehub.member.entity.Member;
-import com.cafehub.cafehub.member.repository.MemberRepository;
 import com.cafehub.cafehub.security.UserDetailsImpl;
 import com.cafehub.cafehub.security.jwt.JwtProvider;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,7 +24,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class BookmarkServiceImpl implements BookmarkService{
+public class BookmarkServiceImpl implements BookmarkService {
 
     //궁금한게.. -> request 다 필요한거 아닌가..?
     private final BookmarkRepository bookmarkRepository;
@@ -33,7 +33,7 @@ public class BookmarkServiceImpl implements BookmarkService{
 
     //북마크 조회
     @Override
-    public ResponseDto<?> getBookmarkList(HttpServletRequest request){
+    public ResponseDto<?> getBookmarkList(HttpServletRequest request) {
         /**
          * 추후 로그인 여부 확인
          */
@@ -41,19 +41,19 @@ public class BookmarkServiceImpl implements BookmarkService{
 
         //3중 조인으로 해결할 것.
         List<Bookmark> bookmarks = bookmarkRepository.findAllByMemberId(member.getId());
-        List<BookmarkedCafeDetails> cafeDetails =  bookmarks.stream()
-                            .map(bookmark -> {
-                                Cafe cafe = bookmark.getCafe();
-                                return BookmarkedCafeDetails.builder()
-                                        .cafeId(cafe.getId())
-                                        .cafeRating(cafe.getRating())
-                                        .cafeTheme(cafe.getTheme().getName())
-                                        .cafePhotoUrl(cafe.getCafePhotoUrl())
-                                        .cafeReviewNum(cafe.getReviewCount())
-                                        .cafeName(cafe.getName())
-                                        .build();
-                            })
-                            .toList();
+        List<BookmarkedCafeDetails> cafeDetails = bookmarks.stream()
+                .map(bookmark -> {
+                    Cafe cafe = bookmark.getCafe();
+                    return BookmarkedCafeDetails.builder()
+                            .cafeId(cafe.getId())
+                            .cafeRating(cafe.getRating())
+                            .cafeTheme(cafe.getTheme().getName())
+                            .cafePhotoUrl(cafe.getCafePhotoUrl())
+                            .cafeReviewNum(cafe.getReviewCount())
+                            .cafeName(cafe.getName())
+                            .build();
+                })
+                .toList();
 
         return ResponseDto.success(cafeDetails);
     }
@@ -63,11 +63,16 @@ public class BookmarkServiceImpl implements BookmarkService{
     @Override
     public ResponseDto<?> saveBookmark(BookmarkRequestDto bookmarkRequestDto, HttpServletRequest request) {
 
-        Cafe cafe = cafeRepository.findById(bookmarkRequestDto.getCafeId()).get();
+        Long cafeId = bookmarkRequestDto.getCafeId();
+        Cafe cafe = cafeRepository.findById(cafeId).get();
         Member member = getMemberFromJwt(request);
 
+        // if(bookmarkRepository.existsByMemberIdAndCafeId(1L, request.getCafeId())) throw new MemberBookmarkAlreadyExistException();
 
-//         if(bookmarkRepository.existsByMemberIdAndCafeId(1L, request.getCafeId())) throw new MemberBookmarkAlreadyExistException();
+        //이미 북마크에 존재하는 카페인 경우
+        if (bookmarkRepository.existsByCafeIdAndMemberId(member.getId(), cafeId)) {
+            return ResponseDto.success(cafeId);
+        }
 
         Bookmark bookmark = Bookmark.builder()
                 .member(member)
@@ -88,17 +93,19 @@ public class BookmarkServiceImpl implements BookmarkService{
         /**
          * 추후 로그인 여부 확인
          */
-      Member member = getMemberFromJwt(request);
+        Member member = getMemberFromJwt(request);
 
+        //Member member = memberRepository.getOne(2l);
         Cafe cafe = cafeRepository.findById(bookmarkRequestDto.getCafeId()).orElse(null);
 
         //해당하는 cafe가 없는 경우-> 에러내용은 내가 작성?
         if (cafe == null) {
-            return ResponseDto.success(0);//fail(ErrorCode.에러내용);
+            return ResponseDto.fail(ErrorCode.CAFE_NOT_FOUND);
         }
         //원하는 카페 아이디가 실제 카페 아이디와 다른 경우->?이게 모지, 질문
+        //서버에서 가지고 있는 정보와 클라이언트가 원하는 정보가 일치하는지 확인
         else if (!bookmarkRequestDto.getCafeId().equals(cafe.getId())) {
-            return ResponseDto.success(0);//return ResponseDto.fail(ErrorCode.에러내용);
+            return ResponseDto.fail(ErrorCode.INVALID_CAFE_ID);
         }
         //일치하는 카페 아이디를 찾은 경우
         else {
@@ -108,7 +115,7 @@ public class BookmarkServiceImpl implements BookmarkService{
         }
     }
 
-   private Member getMemberFromJwt(HttpServletRequest request) {
+    private Member getMemberFromJwt(HttpServletRequest request) {
         Authentication authentication = jwtProvider.getAuthentication(request.getHeader("Authorization").substring(7));
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         return userDetails.getMember();
