bcf16e1170896d07cc662e83a99261c995a553f2
zakie <realzzaekkii@gmail.com>
2024-06-06 19:17:00 +0900

    [REFACTOR] comment db접근 개선

--- src/main/java/com/cafehub/cafehub/member/repository/MemberRepository.java
+++ src/main/java/com/cafehub/cafehub/member/repository/MemberRepository.java
@@ -2,9 +2,17 @@ package com.cafehub.cafehub.member.repository;
 
 import com.cafehub.cafehub.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
+
+    // 각 댓글의 멤버를 한번에 가져와서, 그 뒤로는 더이상 db에 접근하지 않도록 하기 위함.
+    @Query("SELECT DISTINCT c.member FROM Comment c WHERE c.id IN :commentIds")
+    List<Member> findAllByCommentIdIn(@Param("commentIds") List<Long> commentIds);
 }
