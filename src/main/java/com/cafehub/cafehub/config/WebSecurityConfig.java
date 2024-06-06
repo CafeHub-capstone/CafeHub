705d6559f8761a77d4df1882970cb259d2440ffe
RYUL99 <seunglul1999@gmail.com>
2024-06-06 20:39:40 +0900

    [FIX] CORS 수정

--- src/main/java/com/cafehub/cafehub/config/WebSecurityConfig.java
+++ src/main/java/com/cafehub/cafehub/config/WebSecurityConfig.java
@@ -67,7 +67,7 @@ public class WebSecurityConfig extends WebMvcConfigurationSupport {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(List.of("https://master.d18slmijdq6uhn.amplifyapp.com/"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(List.of("*"));
