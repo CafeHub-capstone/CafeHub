d0a6eafefda9c0ac14988df8f6059bda74671574
RYUL99 <seunglul1999@gmail.com>
2024-06-07 03:09:22 +0900

    fix

---
+++ src/main/java/com/cafehub/cafehub/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.cafehub.cafehub.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://13.209.177.1:3000")
+                // GET, POST, PATCH, DELETE, OPTIONS 메서드 허용
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+}
