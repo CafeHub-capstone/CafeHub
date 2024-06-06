ad087e470f7759a8596e7cdd5989dfaafff209f3
RYUL99 <seunglul1999@gmail.com>
2024-06-07 04:43:22 +0900

    fix

--- src/main/java/com/cafehub/cafehub/member/mypage/dto/ProfileRequestDto.java
+++ src/main/java/com/cafehub/cafehub/member/mypage/dto/ProfileRequestDto.java
@@ -2,9 +2,13 @@ package com.cafehub.cafehub.member.mypage.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ProfileRequestDto {
     private String nickname;
